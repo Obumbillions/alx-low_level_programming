2946264873c93050e27d41e246ef487272a2190a
Obumbillions <obumnemeokoye914@gmail.com>
2022-03-15 11:34:47 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,14 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main - entry point
- * Description: Program that prints putchar
- * Return: 0
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase followed by
+ * Return: Nothing.
  */
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
 	_putchar('\n');
-	return (0);
 }
