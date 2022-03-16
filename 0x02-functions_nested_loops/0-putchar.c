60fb5af622e636c52edb64a0435b22dc0753010a
Obumbillions <obumnemeokoye914@gmail.com>
2022-03-15 18:44:06 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * main - prints _putchar
+ * main - prints _putchar followed by a new line
  *
- * Return: 0 (success)
+ * Return: 0 on success. Erro code otherwise
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char ch[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(ch[i]);
+	}
 	_putchar('\n');
 
 	return (0);
